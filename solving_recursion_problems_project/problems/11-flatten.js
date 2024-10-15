4d2c0dd50c2a4cbe86d2c59535c17f138b71a76d
toeireishuman <141298656+toeireishuman@users.noreply.github.com>
2024-10-15 19:37:42 +0800

    Add my solution to 11-flatten

--- solving_recursion_problems_project/problems/11-flatten.js
+++ solving_recursion_problems_project/problems/11-flatten.js
@@ -10,11 +10,30 @@ flatten([1, 2]); // [1, 2]
 flatten([1, [2, [3]]]); // [1, 2, 3]
 ***********************************************************************/
 
-// your code here
-  
+// (array X or (array X)) -> (array X)
+// Given an array with nested arrays, return an array
+// with all the nested content on one level.
+const flatten = array => {
+	const newArray = [];
+
+	for (let i = 0; i < array.length; i++) {
+		const element = array[i];
+
+		if (Array.isArray(element)) {
+			newArray.push(...flatten(element));
+		} else {
+			newArray.push(element);
+		}
+	}
+
+	return newArray;
+};
+
+console.log(flatten([1, [2, [3, [4, [5, [6, 7], 8]]]]]));
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
-  module.exports = flatten;
+	module.exports = flatten;
 } catch (e) {
-  module.exports = null;
+	module.exports = null;
 }
