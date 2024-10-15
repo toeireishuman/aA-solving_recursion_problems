a7ebe09504b472806e5bd4a98c42f2e7d6c70ce6
toeireishuman <141298656+toeireishuman@users.noreply.github.com>
2024-10-15 16:53:45 +0800

    Add my solution to 07-exponent

--- solving_recursion_problems_project/problems/07-exponent.js
+++ solving_recursion_problems_project/problems/07-exponent.js
@@ -1,5 +1,5 @@
 /***********************************************************************
-Write a recursive function called `exponent` that takes two integers, 
+Write a recursive function called `exponent` that takes two integers,
 `num` and `power`, and returns `num` raised to the `power`th power. Your
 function should work when `num` or `power` are positive OR negative.
 
@@ -20,13 +20,35 @@ exponent(2, -2); // 1/4 (or 0.25)
 exponent(5, 5); // 3125
 ***********************************************************************/
 
-function exponent(b, n) {
-  // your code here
+// // Number Number -> Number
+// // Given a base and an exponent, return the
+// // base raised to the exponent.
+// function exponent(b, n) {
+// 	if (n === 0) {
+// 		return 1;
+// 	} else if (n < 0) {
+// 		return (1 / b) * exponent(b, n + 1);
+// 	} else {
+// 		return b * exponent(b, n - 1);
+// 	}
+// }
+
+// Number Number -> Number
+// Given a base and an exponent, return the
+// base raised to the exponent.
+function exponent(b, n, result = 1) {
+	if (n === 0) {
+		return result;
+	} else if (n < 0) {
+		return exponent(b, n + 1, result * (1 / b));
+	} else {
+		return exponent(b, n - 1, result * b);
+	}
 }
-  
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
-  module.exports = exponent;
+	module.exports = exponent;
 } catch (e) {
-  module.exports = null;
+	module.exports = null;
 }
