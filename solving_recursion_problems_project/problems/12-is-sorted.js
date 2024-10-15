baa7979ca987b05c6ff196f2d40252832a38981a
toeireishuman <141298656+toeireishuman@users.noreply.github.com>
2024-10-15 19:42:42 +0800

    Add my solution to 12-is-sorted

--- solving_recursion_problems_project/problems/12-is-sorted.js
+++ solving_recursion_problems_project/problems/12-is-sorted.js
@@ -10,11 +10,27 @@ isSorted([2, 4, 6, 7, 8]); // true
 isSorted([5, 4, 3, 2, 1]); // false
 ***********************************************************************/
 
-// your code here
+// (array Number) -> Boolean
+// Given an array of numbers, return true if the
+// numbers are in ascending order.
+isSorted = function (array) {
+	if (array.length === 1) {
+		return true;
+	} else {
+		const number1 = array[0];
+		const number2 = array[1];
+
+		if (number1 > number2) {
+			return false;
+		} else {
+			return isSorted(array.slice(1));
+		}
+	}
+};
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
-  module.exports = isSorted;
+	module.exports = isSorted;
 } catch (e) {
-  module.exports = null;
+	module.exports = null;
 }
