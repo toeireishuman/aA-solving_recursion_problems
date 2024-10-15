b830f06592214f055a6a945e22cac8eaa2815b25
toeireishuman <141298656+toeireishuman@users.noreply.github.com>
2024-10-15 16:23:22 +0800

    Add my solution to 02-sum-array

--- solving_recursion_problems_project/problems/02-sum-array.js
+++ solving_recursion_problems_project/problems/02-sum-array.js
@@ -10,11 +10,29 @@ sumArray([0, 1, -3]); //  -2
 sumArray([1, 2, 3, 4, 5]); // 15
 ***********************************************************************/
 
-// your code here
-  
+// // (array Number) -> Number
+// // Given an array of numbers, return the sum of all numbers.
+// function sumArray(array) {
+// 	if (array.length === 0) {
+// 		return 0;
+// 	} else {
+// 		return array[0] + sumArray(array.slice(1));
+// 	}
+// }
+
+// (array Number) -> Number
+// Given an array of numbers, return the sum of all numbers.
+function sumArray(array, current_sum = 0) {
+	if (array.length === 0) {
+		return current_sum;
+	} else {
+		return sumArray(array.slice(1), current_sum + array[0]);
+	}
+}
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
-  module.exports = sumArray;
+	module.exports = sumArray;
 } catch (e) {
-  module.exports = null;
+	module.exports = null;
 }
