b2ef2e33b918692d4412a23bf83e792cb329cdb8
toeireishuman <141298656+toeireishuman@users.noreply.github.com>
2024-10-18 02:46:03 +0800

    Add my solution to 17-bonus-permutations

--- solving_recursion_problems_project/problems/17-bonus-permutations.js
+++ solving_recursion_problems_project/problems/17-bonus-permutations.js
@@ -12,7 +12,33 @@ permutations([1, 2, 3]) // [[1, 2, 3], [1, 3, 2],
                         // [3, 1, 2], [3, 2, 1]]
 ***********************************************************************/
 
-// your code here
+const permutations = function (array) {
+	const allPermutations = [];
+
+	if (array.length <= 1) {
+		allPermutations.push(...array);
+	} else if (array.length === 2) {
+		allPermutations.push(...[array, [array[1], array[0]]]);
+	} else {
+		for (let i = 0; i < array.length; i++) {
+			const oddOneOut = array[i];
+			const rest = permutations([
+				...array.slice(0, i),
+				...array.slice(i + 1),
+			]);
+
+			rest.forEach(element => {
+				allPermutations.push([oddOneOut, ...element]);
+			});
+		}
+	}
+	return allPermutations;
+};
+
+// // permutations([1, 2]); // [[1, 2], [2, 1]]
+// console.log(permutations([1, 2, 3])); // [[1, 2, 3], [1, 3, 2],
+// // [2, 1, 3], [2, 3, 1],
+// // [3, 1, 2], [3, 2, 1]]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
